ffe379ab70b0a85acf48423cbbae7f1214f74cf5
ahmetizgi <dev.ahmetizgi@gmail.com>
2021-02-16 12:25:22 +0300

    TrackDetailScreen
    Mapview and Polyline added

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Context as TrackContext } from "../context/TrackContext";
+import MapView, { Polyline } from "react-native-maps";
 
 const TrackDetailScreen = ({ route }) => {
   const { state } = useContext(TrackContext);
@@ -8,10 +9,21 @@ const TrackDetailScreen = ({ route }) => {
   const { _id } = route.params;
 
   const track = state.find((t) => t._id === _id);
+  const initialCoords = track.locations[0].coords;
 
   return (
     <View>
       <Text style={styles.text}> {track.name} </Text>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+          ...initialCoords,
+        }}
+      >
+        <Polyline coordinates={track.locations.map((loc) => loc.coords)} />
+      </MapView>
     </View>
   );
 };
@@ -21,5 +33,8 @@ export default TrackDetailScreen;
 const styles = StyleSheet.create({
   text: {
     fontSize: 36,
+  },
+  map: {
+    height: 300,
   },
 });
